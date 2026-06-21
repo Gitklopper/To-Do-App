5dba67a3fe8894cda8e24b746998d3f4cc947c7d
ombilica <ombilica@student.42heilbronn.de>
2026-06-22 01:41:09 +0200

    small main to check for compiling

--- Backend/main.go
+++ Backend/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
-	"net/http"
 )
 
 func main() {
-
+	addTodo("test")
+	test, err := findTodoByID(1)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(test)
+	setPriority(1, PriorityUrgent)
+	setStatus(1, ProgressionInProgress)
+	fmt.Println(test)
 }
