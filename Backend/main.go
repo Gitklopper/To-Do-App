653c6f49b90d6c8bea9984c2d68d8fd537e40103
ombilica <ombilica@student.42heilbronn.de>
2026-06-21 18:46:13 +0200

    change ex, include splice-literal and map for pagecount

--- Backend/main.go
+++ Backend/main.go
@@ -20,9 +20,31 @@ func (b Book) Summary() string {
 	return fmt.Sprintf("%s (%d pages) - %s", b.Title, b.Pages, status)
 }
 
+func CountUnread(myBooks []Book) int {
+	count := 0
+
+	for _, book := range myBooks {
+		if !book.IsRead {
+			count++
+		}
+	}
+	return count
+}
+
 func main() {
-	myBook := Book {Title: "Moby Dick", Pages : 150, IsRead: false}
-	fmt.Println(myBook.Summary())
-	myBook.MarkAsRead()
-	fmt.Println(myBook.Summary())
+	myBooks := []Book{ 
+		{Title: "Moby Dick", Pages: 150, IsRead: true},
+		{Title: "Raytracer Challange", Pages: 361, IsRead: false},
+		{Title: "How to get over a Breakup", Pages: 211, IsRead: false},
+	}
+	pageCount := map[string]int{
+		"Moby Dick": 150,
+		"Raytracer Challange": 361,
+		"How to get over a Breakup": 211,
+	}
+	fmt.Println(CountUnread(myBooks))
+	val, exists := pageCount["Moby Dick"]
+	fmt.Println(val, exists)
+	val, exists = pageCount["Easy"]
+	fmt.Println(val, exists)
 }
