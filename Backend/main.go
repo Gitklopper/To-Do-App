0410b128e67644c38ec5810ae8d2c7d326cbd8aa
ombilica <ombilica@student.42heilbronn.de>
2026-06-21 19:26:33 +0200

    add pointer references and change/swap value of pages

--- Backend/main.go
+++ Backend/main.go
@@ -31,20 +31,35 @@ func CountUnread(myBooks []Book) int {
 	return count
 }
 
+func IncrementPageCount(pages *int) {
+	*pages = *pages + 1
+}
+
+func SwapPageCount(a, b *int) {
+	tmp := *a
+	*a = *b
+	*b = tmp
+}
+
 func main() {
 	myBooks := []Book{ 
 		{Title: "Moby Dick", Pages: 150, IsRead: true},
 		{Title: "Raytracer Challange", Pages: 361, IsRead: false},
 		{Title: "How to get over a Breakup", Pages: 211, IsRead: false},
 	}
-	pageCount := map[string]int{
-		"Moby Dick": 150,
-		"Raytracer Challange": 361,
-		"How to get over a Breakup": 211,
+	pageCount := map[int]*int{
+		0: &myBooks[0].Pages,
+		1: &myBooks[1].Pages,
+		2: &myBooks[2].Pages,
 	}
-	fmt.Println(CountUnread(myBooks))
-	val, exists := pageCount["Moby Dick"]
-	fmt.Println(val, exists)
-	val, exists = pageCount["Easy"]
-	fmt.Println(val, exists)
+	val, exists := pageCount[0]
+	fmt.Println(*val, exists)
+	IncrementPageCount(&myBooks[0].Pages)
+	val, exists = pageCount[0]
+	fmt.Println(*val, exists)
+	fmt.Println(myBooks[0].Pages)
+	fmt.Println(myBooks[1].Pages)
+	SwapPageCount(&myBooks[0].Pages, &myBooks[1].Pages)
+	fmt.Println(myBooks[0].Pages)
+	fmt.Println(myBooks[1].Pages) 
 }
