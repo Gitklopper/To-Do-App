a5bd2167200651d512108915267bf7754edac1e6
ombilica <ombilica@student.42heilbronn.de>
2026-06-21 21:17:52 +0200

    fresh start

--- Backend/main.go
+++ Backend/main.go
@@ -1,69 +1,11 @@
 package main
 
-import "fmt"
-
-type Describable interface {
-	Describe() string
-}
-
-type Book struct {
-	Title string
-	Pages int
-	IsRead bool
-}
-type Movie struct {
-	Title string
-	Length int
-}
-
-func (b *Book) MarkAsRead() {
-	b.IsRead = true
-}
-
-func (b Book) Describe() string {
-	status := "Unread"
-	if b.IsRead {
-		status = "Read"
-	}
-	return fmt.Sprintf("%s (%d pages) - %s", b.Title, b.Pages, status)
-}
-
-func (m Movie) Describe() string {
-	return fmt.Sprintf("%s (%d min)", m.Title, m.Length)
-}
-
-func PrintDescription(s Describable) {
-	fmt.Println(s.Describe())
-}
-
-func CountUnread(myBooks []Book) int {
-	count := 0
-
-	for _, book := range myBooks {
-		if !book.IsRead {
-			count++
-		}
-	}
-	return count
-}
-
-func IncrementPageCount(pages *int) {
-	*pages++
-}
-
-func SwapPageCount(a, b *int) {
-	tmp := *a
-	*a = *b
-	*b = tmp
-}
+import (
+	"fmt"
+	"encoding/json"
+	"net/http"
+)
 
 func main() {
-	myBooks := []Book{ 
-		{Title: "Moby Dick", Pages: 150, IsRead: true},
-		{Title: "Raytracer Challange", Pages: 361, IsRead: false},
-		{Title: "How to get over a Breakup", Pages: 211, IsRead: false},
-	}
-	myMovie := Movie{Title: "Idiocracy", Length: 220}
-	PrintDescription(myBooks[0])
-	PrintDescription(myMovie)
+
 }
